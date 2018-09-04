a72a709014f9f7e7600b694386374a4fd37bd3c2
Aluno <Aluno@Digital.net>
2018-09-04 18:07:37 -0300

    inicio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using exgattiASPNET.Models;
+
+namespace exgattiASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExIdade()
+        {
+            return View();
+        }
+        public IActionResult ExPecas()
+        {
+            return View();
+        }
+        public IActionResult ExCalculo()
+        {
+            return View();
+        }
+        public IActionResult ExSoma()
+        {
+            return View();
+        }
+        public IActionResult ExIdadeSexo()
+        {
+            return View();
+        }
+        public IActionResult ExAreaTriangulo()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exgattiASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
